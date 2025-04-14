6b97ecd83227264a779ecb6fde2ac91c13771d94
prathamhingu <prathamhingu94@gmail.com>
2025-04-14 22:02:32 +0530

    Add files via upload

---
+++ practical1-1.cpp
@@ -0,0 +1,77 @@
+/* Programmer : Pratham Hingu
+   Aim : To create a secure OOP-based banking system for managing accounts
+   Date : 07 February, 2025
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class BankAccount
+{
+    string accountHolderName;
+    long int accountNumber;
+    long int balance;
+
+public:
+
+    void getAccountDetails()
+    {
+        cout << "Enter the name of account holder: ";
+        cin.ignore(); // To clear any leftover newline character from input buffer
+        getline(cin, accountHolderName);
+
+        cout << "Enter the account number: ";
+        cin >> accountNumber;
+
+        cout << "Enter the current balance of this account: ";
+        cin >> balance;
+    }
+
+    void deposit(long int amount)
+    {
+        balance += amount;
+        cout << "Amount deposited: " << amount << endl;
+    }
+
+    void withdraw(long int amount)
+    {
+        if (balance >= amount)
+        {
+            balance -= amount;
+            cout << "Amount withdrawn: " << amount << endl;
+        }
+        else
+        {
+            cout << "Insufficient balance." << endl;
+        }
+    }
+
+    void displayBalance() const
+    {
+        cout << "Your current balance is: " << balance << endl;
+    }
+};
+
+int main()
+{
+    BankAccount b1;
+    long int amount;
+
+    b1.getAccountDetails();
+
+    cout << "Enter the amount you want to deposit: ";
+    cin >> amount;
+    b1.deposit(amount);
+
+    cout << "Enter the amount you want to withdraw: ";
+    cin >> amount;
+    b1.withdraw(amount);
+
+    b1.displayBalance();
+
+    cout << endl;
+    cout << "Pratham Hingu :: 24CE039" << endl;
+
+    return 0;
+}
